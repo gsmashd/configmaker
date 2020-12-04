fb9e44d53188f53fdfa9ecf285adcf83110fc568
Geir A <geiramh@stud.ntnu.no>
2020-12-04 14:53:14 +0100

    typo + pep 8

--- setup.py
+++ setup.py
@@ -18,6 +18,6 @@ setup(
     url = "https://github.com/gcfntnu/gcf-tools",
     scripts = ['configmaker/configmaker.py', 'testdata/create_testdata.py'],
     packages=['configmaker', 'testdata'],
-    install_requiress=['glob', 're', 'pandas', 'argparse', 'logging','xlrd>=1.0.0'],
-    setup_requiress=['glob', 're', 'pandas', 'argparse', 'logging','xlrd>=1.0.0'],
+    install_requires=['glob', 're', 'pandas', 'argparse', 'logging', 'xlrd>=1.0.0'],
+    setup_requires=['glob', 're', 'pandas', 'argparse', 'logging', 'xlrd>=1.0.0'],
 )
